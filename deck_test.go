a0cf19b2f377a3fd80226527e7016c53fb1e7b04
Vansh <patelvansh1023@gmail.com>
2022-03-15 11:32:26 -0400

    Initial commit

---
+++ deck_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("ecpected a")
+
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, go %v", len(loadedDeck))
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// Goddamnit
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+
+}
